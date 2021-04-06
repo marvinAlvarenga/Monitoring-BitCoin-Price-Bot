8353db89f26f201a191f0a78fbce4ea6507bcd7e
Marvin Alvarenga <marvinalexz@gmail.com>
2021-04-05 22:35:33 -0600

    ✨ Send reports to Telegram Chat

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import requests
 import schedule
 from bs4 import BeautifulSoup
 
-from environment import BASE_URL
+from environment import BASE_URL, BIT_COIN_URL
 
 
 def send_request(url):
@@ -18,5 +18,28 @@ def send_message(message):
     send_request(url)
 
 
+def get_bit_coin_price():
+    response = send_request(BIT_COIN_URL)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    result = soup.find('td', {'class': 'wbreak_word align-middle coin_price'})
+
+    return result.text
+
+
+def report_bit_coin_price():
+    btc_price = get_bit_coin_price()
+
+    message = f'The current price of Bit Coin is: {btc_price}'
+    send_message(message=message)
+
+
+def schedule_report():
+    schedule.every().minute.at(':00').do(report_bit_coin_price)
+
+
 if __name__ == '__main__':
-    send_message('Hola soy marvin!')
+    schedule_report()
+
+    while True:
+        schedule.run_pending()
